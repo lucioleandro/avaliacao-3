577ebe3a62c97c50877511ca64161e25e7a7724f
Lúcio Leandro <lucio.leo85@gmail.com>
2021-12-15 13:17:43 -0300

    COments do composite

--- src/composite/Cursavel.java
+++ src/composite/Cursavel.java
@@ -1,6 +1,9 @@
 package composite;
 
 public interface Cursavel {
+	
+	// Não coloquei apenas o getPreco pois além do cálculo do preço cursaveis tbm tem a caracteristica em comum
+	// do cálculo da chCumproda e PctCumprida
 	double getPreco();
 	double getCHCumprida();
 	double getPctCumprido();
